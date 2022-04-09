80e178ae7faee3f0e2a08f9c92729007f1d170e7
ajju40959@gmail.com <ajju40959@gmail.com>
2022-04-10 05:10:26 +0600

    t8cit

--- solana/src/pages/_app.tsx
+++ solana/src/pages/_app.tsx
@@ -4,64 +4,33 @@ import { FC } from 'react';
 import { ContextProvider } from '../contexts/ContextProvider';
 import { AppBar } from '../components/AppBar';
 import { ContentContainer } from '../components/ContentContainer';
-import { Layout } from '../components/layout';
+import { Footer } from '../components/Footer';
 import Notifications from '../components/Notification'
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const App: FC<AppProps> = ({ Component, pageProps }) => {
+const App: FC < AppProps > = ({ Component, pageProps }) => {
     return (
         <>
           <Head>
             <title>Solana Scaffold Lite</title>
           </Head>
 
-
-
-          
-            <Layout>
-          
-         
+          <ContextProvider>
+            <div className="flex flex-col h-screen">
               <Notifications />
-            
+              <AppBar/>
+              <ContentContainer>
                 <Component {...pageProps} />
-     
-         
-            
-            </Layout>
-            
-          
-          
-          
-          
+              </ContentContainer>
+              <Footer/>
+            </div>
+          </ContextProvider>
         </>
     );
 };
 
 export default App;
-
-
-
-
-
 
 
 
